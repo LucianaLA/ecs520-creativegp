f7da32eb73d4349b42fa87b619b2b1d03cf1b99b
Luciana <l.lopesdealmeida25@gmail.com>
2023-03-03 13:44:49 +0000

    basic arduino code added to

--- arduino/microphonetest-CGP/microphonetest-CGP.ino
+++ arduino/microphonetest-CGP/microphonetest-CGP.ino
@@ -1,13 +1,30 @@
 int microphonePin = 9;
 int state = 0;
 int mappedState;
+int POTENTIOMETER_PIN = 12;
+int ledStrip = 5;
 
 void setup() {
   pinMode(microphonePin, INPUT);
+  pinMode(POTENTIOMETER_PIN, INPUT);
+  pinMode(ledStrip, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
+  int pVal = map(potentiometerValue, 0, 1023, 0, 40);
+  if(pVal<10){
+    digitalWrite(5,HIGH);
+  } else if (10<pVal<20){
+    digitalWrite(5,LOW);
+  } else if (20<pVal<30){
+    digitalWrite(5,HIGH);
+  } else{
+    digitalWrite(5,HIGH);
+  }
+
+
   state = analogRead(microphonePin);
   //String stateString = String(state);
   //mappedState = map(state, 0, 10, 240,250);
